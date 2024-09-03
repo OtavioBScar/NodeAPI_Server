86f9996569332040e16144df6a50c4016ad4e853
OtavioBScar <otavio.scarpelline@gmail.com>
2024-09-03 13:20:55 -0300

    feat: Controllers, models and routes code, using PrismaORM

--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 * Projeto para testes
 
 * utilizando javascript no framework NodeJS
+
+* .env
+PORT=your_port
--- src/server.js
+++ src/server.js
@@ -1,12 +1,11 @@
 import express from "express"
 import "dotenv/config"
+import routes from "./routes/index.js"
 
 const application = express()
 const PORT = process.env.port
 
-application.get("/users", (req, res) => {
-    res.send(users)
-})
+application.use("/api", routes)
 
 application.listen(PORT)
 console.log(`Server running on http://localhost:${PORT}`)
